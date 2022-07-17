cd81a4ada4357ceb98cc659c9ecb5051f72d64ad
thomaswhb <thomaswhb@gmail.com>
2022-07-17 21:19:45 +1000

    add bin bash

--- main.tf
+++ main.tf
@@ -9,10 +9,22 @@ resource "aws_instance" "example" {
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
-              nohup busybox httpd -f -p ${var.server_port} &
+              nohup busybox httpd -f -p 8080 &
               EOF
 
   tags = {
     Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
