574671b2eb5f4ecb87d69636747a21b0e3b35225
thomaswhb <thomaswhb@gmail.com>
2022-07-17 21:09:03 +1000

    added user data bash

--- main.tf
+++ main.tf
@@ -6,6 +6,12 @@ resource "aws_instance" "example" {
   ami           = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
 
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+
   tags = {
     Name = "terraform-example"
   }
